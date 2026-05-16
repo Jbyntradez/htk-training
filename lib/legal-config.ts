6facceb3bf9ef5c42fc5f9f5cf66865c65336681
Jimmy Bynum <jbyntradez@Jimmys-MacBook-Pro.local>
2026-05-15 19:04:01 -0700

    Update legal contact email references

--- lib/legal-config.ts
+++ lib/legal-config.ts
@@ -1,4 +1,4 @@
-export const HTK_LEGAL_CONTACT_EMAIL = "jimmy.bynum.06@gmail.com";
+export const HTK_LEGAL_CONTACT_EMAIL = "legal@htktrainingco.com";
 
 export const HTK_LEGAL_LAST_UPDATED = "May 7, 2026";
 
